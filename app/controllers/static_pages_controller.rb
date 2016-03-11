c97e38764efb47bca0e781657d09ae28ad3f8c10
ADAM STONE <astone13@students.towson.edu>
2016-03-10 23:07:45 -0500

    added profile and edit profile pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,10 @@ class StaticPagesController < ApplicationController
   
   def login
   end
+  
+  def profile
+  end
+  
+  def editprofile
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/contact' => 'static_pages#contact'
   get '/register' => 'static_pages#register'
   get '/login' => 'static_pages#login'
+  get '/profile' => 'static_pages#profile'
+  get '/editprofile' => 'static_pages#editprofile'
 
   root 'static_pages#home'
 
