0152fa47eae33dcef8574d54b2a1d7015a71f3bd
Christian Hogan <1cph93@gmail.com>
2016-03-21 15:58:15 -0400

    Create initial Project model

--- app/models/vacant.rb
+++ app/models/vacant.rb
@@ -1,5 +1,7 @@
 class Vacant < ActiveRecord::Base
 
+  has_many :projects, dependent: :destroy
+
   # Returns the vacants whose address contain one or more words that form the query
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
