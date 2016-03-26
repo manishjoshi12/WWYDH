01ee89574e55c7556327b011c584e63f6ad95037
Christian Hogan <1cph93@gmail.com>
2016-03-26 12:46:20 -0400

    Add advanced search feature to vacants index page

--- app/controllers/vacants_controller.rb
+++ app/controllers/vacants_controller.rb
@@ -3,13 +3,22 @@ class VacantsController < ApplicationController
 	skip_before_action :ensure_login, only: [:index, :show]
 
 	def index
-    if params[:search]
-      @vacants = Vacant.search(params[:search]).order("id ASC")
+    # if params[:search]
+		# if params.has_key?(:neighborhood)
+			@vacants = Vacant.search(params).order("id ASC")
 											 .paginate(page: params[:page], per_page: 10)
-    else
-      @vacants = Vacant.order("id ASC")
-											 .paginate(page: params[:page], per_page: 10)
-    end
+
+		# if params.has_key?(:neighborhood)
+		# 	@vacants = Vacant.where("fulladdress LIKE ? AND neighborhood LIKE ?", "%#{params[:fulladdress]}%", "%#{params[:neighborhood]}%")
+		# 									 .order("id ASC").paginate(page: params[:page], per_page: 10)
+	  # elsif params.has_key?(:search)
+    #  	@vacants = Vacant.search(params[:search]).order("id ASC")
+		# 									 .paginate(page: params[:page], per_page: 10)
+		# else
+    #   @vacants = Vacant.order("id ASC")
+		# 									 .paginate(page: params[:page], per_page: 10)
+    # end
+
   end
 
 	def show
@@ -19,5 +28,5 @@ class VacantsController < ApplicationController
 	def vacant_params
 		params.require(:vacant).permit(:fulladdress, :policedistrict)
 	end
-	
+
 end
--- app/models/vacant.rb
+++ app/models/vacant.rb
@@ -3,9 +3,25 @@ class Vacant < ActiveRecord::Base
   has_many :projects, dependent: :destroy
 
   # Returns the vacants whose address contain one or more words that form the query
-  def self.search(query)
-    # where(:title, query) -> This would return an exact match of the query
-    where("fulladdress like ?", "%#{query.upcase}%")
+  # def self.search(query)
+  #   # where(:title, query) -> This would return an exact match of the query
+  #   find(:all) unless query
+  #
+  #   if query
+  #     where("fulladdress like ?", "%#{query.upcase}%")
+  #   end
+  # end
+
+  def self.search(params)
+    if params.empty?
+      find(:all)
+    elsif !params.has_key?(:neighborhood)
+      where("fulladdress LIKE ?", "%#{params[:fulladdress].upcase}%")
+    else
+      where("fulladdress LIKE ? AND neighborhood LIKE ? AND policedistrict LIKE ?",
+            "%#{params[:fulladdress].upcase}%", "%#{params[:neighborhood].upcase}%",
+            "%#{params[:policedistrict].upcase}%")
+    end
   end
 
 end
