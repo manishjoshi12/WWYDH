438706c348c63927d56939e7df64e58b74d99fea
Christian Hogan <1cph93@gmail.com>
2016-04-27 19:49:39 -0400

    Update routes to include / instead of .

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
   get '/about' => 'static_pages#about'
-  get '/profile' => 'static_pages#profile'
-  get '/editprofile' => 'static_pages#editprofile'
+  get '/edit_profile/:id' => 'static_pages#edit_profile', as: 'edit_profile'
   get '/my_projects/:id' => 'users#my_projects', as: 'my_projects'
 
   root 'static_pages#home'
