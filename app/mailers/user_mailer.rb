389f569e3c82c258bc044b6cf996df78eec9d71f
Christian Hogan <1cph93@gmail.com>
2016-05-04 14:25:48 -0400

    Remove first and last name from user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ActionMailer::Base
 
 		def registration_confirmation(user)
 			@user = user
-			mail(:to => "#{user.firstname} #{user.lastname} <#{user.email}>", :subject => "Please confirm your registration")
+			mail(:to => "#{user.username} <#{user.email}>", :subject => "Please confirm your registration")
 		end
 	end
